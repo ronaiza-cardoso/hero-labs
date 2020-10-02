85029ccb15310c900a13db7fb804d8a1d451f9a1
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-10-02 11:58:41 +0100

    style(*): add global style and fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
 import HeroList from 'pages/HeroList'
 
+import theme from 'styles/theme'
+import GlobalStyles from 'styles/global'
+
 function App() {
-  return <HeroList />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <HeroList />
+    </ThemeProvider>
+  )
 }
 
 export default App
