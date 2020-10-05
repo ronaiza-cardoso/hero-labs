2afa6664e4a43ed19e64ea492c3361a4d08ab7af
ronaiza-cardoso <ronaizacard@gmail.com>
2020-10-05 09:56:57 +0000

    Deploying to gh-pages from  @ a50c9be304248a27a61001d082d784ea4844b605 🚀

---
+++ precache-manifest.d044e6b30537acc075b2d71e71b2d9cf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2be867766eac8a7dd7b1ebf65c5452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2be9409d961b31e1993",
+    "url": "/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "e2be9409d961b31e1993",
+    "url": "/static/js/2.2dd2d4dc.chunk.js"
+  },
+  {
+    "revision": "908efc0c242a46a807a26bd466ddfd09",
+    "url": "/static/js/2.2dd2d4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e967636dcee9ef81a5",
+    "url": "/static/js/main.61e59378.chunk.js"
+  },
+  {
+    "revision": "0d6f23c069ea5b2d67c3",
+    "url": "/static/js/runtime-main.264ca197.js"
+  },
+  {
+    "revision": "32bf5cc4325bf1e5eea598fd547784d6",
+    "url": "/static/media/book.32bf5cc4.svg"
+  },
+  {
+    "revision": "8352c5c3de31a1cdd01ad88e066ed874",
+    "url": "/static/media/heart-filled.8352c5c3.svg"
+  },
+  {
+    "revision": "61bd6eb829e1e3d6b7729042dfc84f62",
+    "url": "/static/media/heart.61bd6eb8.svg"
+  },
+  {
+    "revision": "627215ec0a5c7ac2870a3c4b9815f42b",
+    "url": "/static/media/logo-transparent-background.627215ec.svg"
+  },
+  {
+    "revision": "55c7f3ac08d9ec989a6ca5d0833a96fb",
+    "url": "/static/media/logo.55c7f3ac.svg"
+  },
+  {
+    "revision": "9652ff449b6b70e68d0a89b5a865b913",
+    "url": "/static/media/loupe.9652ff44.svg"
+  },
+  {
+    "revision": "529229ac3b4406539cf596297b447ee4",
+    "url": "/static/media/noun-hero.529229ac.svg"
+  },
+  {
+    "revision": "fc8414eac6df20a323773c6d376ae635",
+    "url": "/static/media/star-filled-disabled.fc8414ea.svg"
+  },
+  {
+    "revision": "a196f94863ca8548c606ada8f2a7f030",
+    "url": "/static/media/video.a196f948.svg"
+  }
+]);
